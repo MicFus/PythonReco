8aca2eb0d37279b922be37be335f451cd55317e3
Michal Fusiarz <michal.fusiarz@avanade.com>
2022-01-21 19:23:52 +0100

    define train and predict endpoints

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello, SmartNinja!"
+@app.route("/train")
+def testEndpoint():
+
+    return "Hello, this is test endpoint"
+
+
+@app.route("/predict")
+def predictEndpoint():
+    user = request.args.get('userId')
+    return f"Hello, this is predict endpoint with {user} id"
 
 
 if __name__ == '__main__':
